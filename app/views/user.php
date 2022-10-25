9e2e0079fc5a47c0f20d5fbd4a27c66cc3c9099a
karvox <judson_borges@hotmail.com>
2022-10-25 11:10:09 -0300

    feat: add update method

--- app/views/user.php
+++ app/views/user.php
@@ -1,10 +1,13 @@
 <?php $this->layout('master', ['title' => $title]) ?>
 
-<?php $this->start('css') ?>
+<h1>User</h1>
 
-    <link rel="stylesheet" href="/css/style.css">
+<form action="/user/update/12" method="post">
+    
+    <input type="text" name="firestName" value="Judson">
+    <input type="text" name="lastName" value="Borges">
+    <input type="email" name="email" value="judsonborges@email.com">
+    <input type="password" name="password" value="123456789">
 
-<?php $this->stop() ?>
-
-<h1>User Profile</h1>
-<p>Hello, <?php echo $this->e($name) ?></p>
+    <button type="submit">Atualizar</button>
+</form>
